cb7e412b1dd6c19695ba04a5e1e4d336999d98fe
sayeediy <153652781+sayeediy@users.noreply.github.com>
2024-03-06 12:27:55 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher
+
+import asyncio
+import configg
+
+
+bot = Bot(configg.TOKEN)
+dp = Dispatcher(bot)
+
+async def main():
+    from handlers import dp
+    try:
+        await dp.start_polling()
+    finally:
+        await bot.session.close()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print('Bot stopped!')
